b76d7d80baf728fbf73197f5c7daa1a80e622795
Alex Barcelo <alex@betarho.net>
2018-02-27 11:12:01 +0100

    minor changes on scripts

--- demos/wordcount/1_RegisterModel.sh
+++ demos/wordcount/1_RegisterModel.sh
@@ -7,21 +7,6 @@ APP=Wordcount
 SRCPATH="src/model"
 STUBSPATH="stubs"
 
-
-
-##### Checks and derived variables definition
-
-# Check dataClay lib
-if [ ! -f $DATACLAYLIB ]; then
-	echo "[ERROR] dataClay client lib (or link) not found at $DATACLAYLIB."
-	exit -1
-fi
-
-# Check toolspath
-if [ ! -f $TOOLSPATH ]; then
-	echo "[ERROR] Bad tools path. Edit TOOLSPATH script variable to change it."
-	exit -1
-fi
 # Local variables
 if [ ! -z $1 ]; then
 	NAMESPACE=$1
@@ -47,7 +32,7 @@ $TOOLSPATH NewDataContract $USER $PASS $DATASET $USER
 
 printMsg "Compile and register model"
 TMPBINPATH=`mktemp -d`
-javac -cp lib/dataclayclient.jar `find $SRCPATH | grep java` -d $TMPBINPATH
+javac -cp $DATACLAYLIB `find $SRCPATH | grep java` -d $TMPBINPATH
 $TOOLSPATH NewModel $USER $PASS $NAMESPACE $TMPBINPATH java
 rm -Rf $TMPBINPATH
 
