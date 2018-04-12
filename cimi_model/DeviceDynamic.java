d2b372420092c52ec57018f4cbf73adebf7df23b
Souvik Sengupta <32735609+ssgUPC@users.noreply.github.com>
2018-04-13 01:29:20 +0200

    Update DeviceDynamic.java

--- cimi_model/DeviceDynamic.java
+++ cimi_model/DeviceDynamic.java
@@ -9,30 +9,31 @@ public class DeviceDynamic extends CIMIResource {
 	//If it contains nested info, it is implemented as a Map<String, Object>
 	//where String is the field name, and Object is the value
 	private Device device;
-	private boolean isLeader;
-	private String ramUnits;
+	//private boolean isLeader;
+	//private String ramUnits;
 	private float ramFree;
 	private float ramFreePercent;
-	private String storageUnits;
+	//private String storageUnits;
 	private int storageFree;
 	private float storageFreePercent;
 	private float cpuFreePercent;
 	private String powerRemainingStatus;
-	private int powerRemainingStatusSeconds; //TODO: It is of type String in resource spec, I think it is a mistake
+	private String powerRemainingStatusSeconds; //TODO: It is of type String in resource spec, I think it is a mistake => There is a reason to put it as a string in the fog area side when we retrieve this info then we can able to compare the battery enabled devices with the permanent electric source devices  
 	private String ethernetAddress; //TODO: This is here and in Device. I guess it should only be in one place
 	private String wifiAddress; //TODO: This is here and in Device. I guess it should only be in one place
 	private List<Object> ethernetThroughputInfo; //TODO: The type of this field is not defined in the resource spec, so I don't know how to implement it
 	private List<Object> wifiThroughputInfo; //TODO: The type of this field is not defined in the resource spec, so I don't know how to implement it
+	private
 	
 	//Constructor, with a parameter for each attribute in this class and in CIMIResource
-	public DeviceDynamic(Device device, boolean isLeader, String ramUnits, float ramFree, float ramFreePercent, 
+	public DeviceDynamic(Device device, String ramUnits, float ramFree, float ramFreePercent, 
 			String storageUnits, int storageFree, float storageFreePercent, float cpuFreePercent, 
 			String powerRemainingStatus, int powerRemainingStatusSeconds, String ethernetAddress, 
-			String wifiAddress, List<Object> ethernetThroughputInfo, List<Object> wifiThroughputInfo,
+			String wifiAddress, List<Object> ethernetThroughputInfo, List<Object> wifiThroughputInfo, Device myleaderID,  
 			String resourceID, String resourceName, String resourceDescription, String resourceURI) {
 		super(resourceID, resourceName, resourceDescription, resourceURI);
 		this.device = device;
-		this.isLeader = isLeader;
+		//this.isLeader = isLeader;
 		this.ramUnits = ramUnits;
 		this.ramFree = ramFree;
 		this.ramFreePercent = ramFreePercent;
@@ -46,6 +47,7 @@ public class DeviceDynamic extends CIMIResource {
 		this.wifiAddress = wifiAddress;
 		this.ethernetThroughputInfo = ethernetThroughputInfo;
 		this.wifiThroughputInfo = wifiThroughputInfo;
+		this.myleaderID = myleaderID;
 	}
 	
 	//Setters (a setter for each property called "set_propertyname")
@@ -108,12 +110,15 @@ public class DeviceDynamic extends CIMIResource {
 	public void set_wifiThroughputInfo(List<Object> wifiThroughputInfo) {
 		this.wifiThroughputInfo = wifiThroughputInfo;
 	}
+	public void set_myleaderID(Device myleaderID) {
+		this.myleaderID = myleaderID;
+	}
 	
 	//A single getter that returns a Map with all the info in this class and in CIMIResource, called "get_classname_info"
 	public Map<String, Object> get_DeviceDynamic_info() {
 		Map<String, Object> info = getCIMIResourceInfo();
 		info.put("device", this.device);
-		info.put("isLeader", this.isLeader);
+		//info.put("isLeader", this.isLeader);
 		info.put("ramUnits", this.ramUnits);
 		info.put("ramFree", this.ramFree);
 		info.put("ramFreePercent", this.ramFreePercent);
@@ -127,6 +132,7 @@ public class DeviceDynamic extends CIMIResource {
 		info.put("wifiAddress", this.wifiAddress);
 		info.put("ethernetThroughputInfo", this.ethernetThroughputInfo);
 		info.put("wifiThroughputInfo", this.wifiThroughputInfo);
+		info.put("myleaderID", this.myleaderID);
 		return info;
 	}
 
