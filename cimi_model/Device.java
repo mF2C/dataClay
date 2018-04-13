ac33b88f7f0a2f80c111e2817498a20d155868a2
Souvik Sengupta <32735609+ssgUPC@users.noreply.github.com>
2018-04-13 12:41:26 +0200

    Update Device.java
    removed the myleaderID

--- cimi_model/Device.java
+++ cimi_model/Device.java
@@ -21,13 +21,13 @@ public class Device extends CIMIResource {
 	private String networkingStandards;
 	private String ethernetAddress;
 	private String wifiAddress;
-	private String myleaderID;
+	//private String myleaderID;
 	
 	//Constructor, with a parameter for each attribute in this class and in CIMIResource
 	public Device(String deviceID, boolean isLeader, String os, String arch, String cpuManufacturer,
 			int physicalCores, int logicalCores, int cpuClockSpeed, int memory, int storage, 
 			boolean powerPlugged, String networkingStandards, String ethernetAddress, String wifiAddress,
-			String myleaderID, String resourceID, String resourceName, String resourceDescription, String resourceURI) {
+			String resourceID, String resourceName, String resourceDescription, String resourceURI) {
 		super(resourceID, resourceName, resourceDescription, resourceURI);
 		this.deviceID = deviceID;
 		this.isLeader = isLeader;
@@ -43,7 +43,7 @@ public class Device extends CIMIResource {
 		this.networkingStandards = networkingStandards;
 		this.ethernetAddress = ethernetAddress;
 		this.wifiAddress = wifiAddress;
-		this.myleaderID = myleaderID;
+		//this.myleaderID = myleaderID;
 	}
 	
 	//Setters (a setter for each property called "set_propertyname")
@@ -123,7 +123,7 @@ public class Device extends CIMIResource {
 		info.put("networkingStandards", this.networkingStandards);
 		info.put("ethernetAddress", this.ethernetAddress);
 		info.put("wifiAddress", this.wifiAddress);
-		info.put("myleaderID", this.myleaderID);
+		//info.put("myleaderID", this.myleaderID);
 		return info;
 	}
 
