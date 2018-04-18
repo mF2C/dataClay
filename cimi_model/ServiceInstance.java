67f932a50496cc63870108c1eeca464f5eade97a
aqueralt <aqueralt@bsc.es>
2018-04-18 19:13:10 +0200

    Add field user_id in ServiceInstance

--- cimi_model/ServiceInstance.java
+++ cimi_model/ServiceInstance.java
@@ -8,6 +8,7 @@ public class ServiceInstance extends CIMIResource {
 	//An attribute for each field in the CIMI resource spec, with the same name
 	//If it contains nested info, it is implemented as a Map<String, Object>
 	//where String is the field name, and Object is the value
+	private String user_id;
 	private String service_id;
 	private String agreement_id;
 	private String status;
@@ -23,6 +24,7 @@ public class ServiceInstance extends CIMIResource {
 	//Constructor
 	public ServiceInstance(Map<String, Object> objectData) {
 		super(objectData);
+		this.user_id = (String) objectData.get("user_id");
 		this.service_id = (String) objectData.get("service_id");
 		this.agreement_id = (String) objectData.get("agreement_id");
 		this.status = (String) objectData.get("status");
@@ -30,6 +32,10 @@ public class ServiceInstance extends CIMIResource {
 	}
 		
 	//Setters (a setter for each property called "set_propertyname")
+	public void set_user_id(String user_id) {
+		this.user_id = user_id;
+	}
+	
 	public void set_service_id(String service_id) {
 		this.service_id = service_id;
 	}
@@ -49,6 +55,7 @@ public class ServiceInstance extends CIMIResource {
 	//A single getter that returns a Map with all the info in this class and in CIMIResource, called "getCIMIResourceData"
 	public Map<String, Object> getCIMIResourceData() {
 		Map<String, Object> info = super.getCIMIResourceData();
+		if (this.user_id != null) info.put("user_id", this.user_id);
 		if (this.service_id != null) info.put("service_id", this.service_id);
 		if (this.agreement_id != null) info.put("agreement_id", this.agreement_id);
 		if (this.status != null) info.put("status", this.status);
@@ -58,6 +65,7 @@ public class ServiceInstance extends CIMIResource {
 	
 	public void updateAllData(Map<String, Object> data) {
 		super.setCIMIResourceData(data);
+		if (data.get("user_id") != null) set_user_id((String) data.get("user_id"));
 		if (data.get("service_id") != null) set_service_id((String) data.get("service_id"));
 		if (data.get("agreement_id") != null) set_agreement_id((String) data.get("agreement_id"));
 		if (data.get("status") != null) set_status((String) data.get("status"));
