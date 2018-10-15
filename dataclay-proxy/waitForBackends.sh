38411742714289947147673b026b3af425b41958
Jonathan Martí <jonathan.marti@bsc.es>
2018-10-15 17:19:04 +0200

    Removed unnecessary print

--- dataclay-proxy/waitForBackends.sh
+++ dataclay-proxy/waitForBackends.sh
@@ -10,7 +10,6 @@ DATACLAY_TOOL=../tool/dClayTool.sh
 DSCOUNTER=1
 while [ $DSCOUNTER -le $NUMBER_OF_SL ]; do
   NUMBER_OF_EE=`$DATACLAY_TOOL GetBackends $DATACLAY_ADMIN_USER $DATACLAY_ADMIN_PASSWORD $LANGUAGE | grep -e "^DS$DSCOUNTER" | wc -l`
-  echo $NUMBER_OF_EE
   if [ ${NUMBER_OF_EE:-0} -lt $NUMBER_OF_EE_PER_SL ]; then
     sleep 2
   else
