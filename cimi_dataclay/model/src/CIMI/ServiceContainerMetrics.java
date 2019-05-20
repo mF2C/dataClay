f93d3ea58f030f3e4fe8448680075948da265a94
Román SG <roman.sosa@atos.net>
2019-05-20 10:40:55 +0100

    Add ServiceContainerMetrics model

---
+++ cimi_dataclay/model/src/CIMI/ServiceContainerMetrics.java
@@ -0,0 +1,40 @@
+package CIMI;
+
+import java.util.Map;
+
+@ReplicateInLeader
+@SuppressWarnings("serial")
+public class ServiceOperationReport extends CIMIResource {
+	/** 
+	 * ==== READ THIS! ====
+	 * An attribute for each field in the CIMI resource spec, with the same name and type.
+	 * 
+	 * 1) If it contains nested info or its a href, it is implemented as a Map<String, Object>
+	 *  where String is the field name, and Object is the value
+	 *  
+	 * 2) If you want your class to be shared/visible in the Leader, please annotate the class 
+	 * 		with @ReplicateInLeader 
+	 *  
+	 * 3) if you want to synchronize your field with agent leaders: add following annotations to your field: 
+	 *		@Replication.InMaster and 
+	 * 		@Replication.AfterUpdate(method = "replicateToDataClaysObjectIsFederatedWith", clazz = "dataclay.util.replication.SequentialConsistency")
+	 * 
+	 * 4) WARNING: All fields must be public or no modifier defined.
+	 * 
+	 * 5) All types defined must be Object types (Integer,, List...) and not primitive types (int,...) 
+	 *    WARNING!!: Use Double for floating point numbers due to JSON restrictions
+	 * 
+	 * 6) Once finished, please do a Pull request and after testing and verifications dataClay will publish 
+	 * a new docker image with your changes/new resource model. 
+	 **/
+	Map<String, Object> device_id;
+	String container_id;
+	String start_time;
+	String stop_time;
+
+	// Constructor
+	public ServiceOperationReport(final Map<String, Object> objectData) {
+		super(objectData);
+	}
+
+}
