abfcdb8b8b9906072ccc660ec242835e4167c83c
rsucasas <rsucasas@gmail.com>
2019-10-17 13:36:05 +0200

    service-operation-report replicated to leader

--- data_management/model/src/CIMI/ServiceOperationReport.java
+++ data_management/model/src/CIMI/ServiceOperationReport.java
@@ -3,36 +3,43 @@ package CIMI;
 import java.util.Map;
 
 @SuppressWarnings("serial")
+@ReplicateInLeader
 public class ServiceOperationReport extends CIMIResource {
-	/** 
+	/**
 	 * ==== READ THIS! ====
 	 * An attribute for each field in the CIMI resource spec, with the same name and type.
-	 * 
+	 *
 	 * 1) If it contains nested info or its a href, it is implemented as a Map<String, Object>
 	 *  where String is the field name, and Object is the value
-	 *  
-	 * 2) If you want your class to be shared/visible in the Leader, please annotate the class 
-	 * 		with @ReplicateInLeader 
-	 *  
-	 * 3) if you want to synchronize your field with agent leaders: add following annotations to your field: 
-	 *		@Replication.InMaster and 
+	 *
+	 * 2) If you want your class to be shared/visible in the Leader, please annotate the class
+	 * 		with @ReplicateInLeader
+	 *
+	 * 3) if you want to synchronize your field with agent leaders: add following annotations to your field:
+	 *		@Replication.InMaster and
 	 * 		@Replication.AfterUpdate(method = "replicateToDataClaysObjectIsFederatedWith", clazz = "dataclay.util.replication.SequentialConsistency")
-	 * 
+	 *
 	 * 4) WARNING: All fields must be public or no modifier defined.
-	 * 
-	 * 5) All types defined must be Object types (Integer,, List...) and not primitive types (int,...) 
+	 *
+	 * 5) All types defined must be Object types (Integer,, List...) and not primitive types (int,...)
 	 *    WARNING!!: Use Double for floating point numbers due to JSON restrictions
-	 * 
-	 * 6) Once finished, please do a Pull request and after testing and verifications dataClay will publish 
-	 * a new docker image with your changes/new resource model. 
+	 *
+	 * 6) Once finished, please do a Pull request and after testing and verifications dataClay will publish
+	 * a new docker image with your changes/new resource model.
 	 **/
 	String compute_node_id;
 	Map<String, Object> requesting_application_id;
 	String operation_name;
 	String operation_id;
 	String start_time;
+	@Replication.InMaster
+	@Replication.AfterUpdate(method = "replicateToDataClaysObjectIsFederatedWith", clazz = "dataclay.util.replication.SequentialConsistency")
 	String expected_end_time;
+	@Replication.InMaster
+	@Replication.AfterUpdate(method = "replicateToDataClaysObjectIsFederatedWith", clazz = "dataclay.util.replication.SequentialConsistency")
 	String result;
+	@Replication.InMaster
+	@Replication.AfterUpdate(method = "replicateToDataClaysObjectIsFederatedWith", clazz = "dataclay.util.replication.SequentialConsistency")
 	Double execution_length;
 
 	// Constructor
